c5257ef1e4eb4318a9b163ec9f29c27beb92407e
peffranckpeffan <peffanfranck@gmail.com>
2019-04-10 23:15:10 -0300

    tests

--- common/teste.tcl
+++ common/teste.tcl
@@ -9,27 +9,31 @@
 # # set A [orient $sel [lindex $I 1] {0 1 0}]
 # # $sel move $A
 # # set I [draw principalaxes $sel]
-set all [atomselect top "chain A or chain B"]
+# set all [atomselect top "chain A or chain B"]
+
+# # set angle 0.001
+# # set x [lindex [measure center [atomselect top "chain B"] weight mass] 0]
+
+# # while {abs($x) > 0.001 & $angle<360} { 
+# # 	$all move [transaxis z $angle]
+# #     set angle [expr $angle + 0.001]
+# # 	set x [lindex [measure center [atomselect top "chain B"] weight mass] 0]
+# # }
 
 # set angle 0.001
-# set x [lindex [measure center [atomselect top "chain B"] weight mass] 0]
-
-# while {abs($x) > 0.001 & $angle<360} { 
-# 	$all move [transaxis z $angle]
+# set y [lindex [measure center [atomselect top "chain B"] weight mass] 1]
+# set z [lindex [measure center [atomselect top "chain B"] weight mass] 2]
+# set true 1
+# while {$true == 1} { 
+# 	$all move [transaxis x $angle]
 #     set angle [expr $angle + 0.001]
-# 	set x [lindex [measure center [atomselect top "chain B"] weight mass] 0]
+# 	set y [lindex [measure center [atomselect top "chain B"] weight mass] 1]
+# 	set z [lindex [measure center [atomselect top "chain B"] weight mass] 2]
+# 	if {abs($y) < 0.001 & abs($z) == $z} {
+# 		set true 0
+# 	}
 # }
-
-set angle 0.001
-set y [lindex [measure center [atomselect top "chain B"] weight mass] 1]
-set z [lindex [measure center [atomselect top "chain B"] weight mass] 2]
-set true 1
-while {$true == 1} { 
-	$all move [transaxis x $angle]
-    set angle [expr $angle + 0.001]
-	set y [lindex [measure center [atomselect top "chain B"] weight mass] 1]
-	set z [lindex [measure center [atomselect top "chain B"] weight mass] 2]
-	if {abs($y) < 0.001 & abs($z) == $z} {
-		set true 0
-	}
-}
+set teste "chain A"
+set teste2 "chain B"
+set x [concat $teste " or " $teste2]
+puts $x
--- lib/tcl/split.tcl
+++ lib/tcl/split.tcl
@@ -1,13 +1,12 @@
 mol load pdb $env(pdb_file)
 
-set x [join [concat $env(protein_selection) " or "] "" ]
-set all [atomselect top [join [concat $x $env(ligand_selection)] "" ]]
+set all [atomselect top [concat $env(protein_selection) " or " $env(ligand_selection)]]
 $all moveby [vecinv [measure center $all weight mass]]
 
 package require Orient
 namespace import Orient::orient
 
-set sel [atomselect top [join [concat $x $env(ligand_selection)] "" ]]
+set sel [atomselect top [concat $env(protein_selection) " or " $env(ligand_selection)]]
 set I [draw principalaxes $sel]
 set A [orient $sel [lindex $I 2] {0 0 1}]
 $sel move $A
